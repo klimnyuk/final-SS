19bae987207d8a7868c7cc4e4b7dac1b2eb1a2d5
klimnyuk <aleksey.klimnyuk@gmail.com>
2021-12-10 12:28:30 +0200

    test

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h1>Hello DevOps</h1>
+<h1>Hello DevOps 2021</h1>
 <form action="welcome.php" method="post">
 Firstname: <input type="text" name="first_name"><br>
 Lastname: <input type="text" name="last_name"><br>
@@ -8,5 +8,4 @@ E-mail: <input type="text" name="e_mail"><br>
 <input type="submit">
 </form>
 </body>
-</html> 
-
+</html>
